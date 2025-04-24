8238a02aaabbc681862499cf733c3d7150046ae4
Nelson Barahona <nelsonluisbarahona@gmail.com>
2025-04-24 14:46:15 -0600

    Agregar archivos de proyecto.

---
+++ PracticaParcial Listas Enlazadas/Server/LES.cs
@@ -0,0 +1,52 @@
+﻿namespace PracticaParcial_Listas_Enlazadas.Server
+{
+    public class LES
+    {
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
+
+        public LES()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+        }
+
+        bool EstaVacia()
+        {
+            return  UltimoNodo == null;
+        }
+
+        public string InsetarNodoAlInicio(Nodo nuevoNodo)
+        {
+            if (EstaVacia()) 
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else 
+            { 
+            nuevoNodo = PrimerNodo.Referencia;
+            PrimerNodo = nuevoNodo;
+			}
+			return $"Se creo el nodo {nuevoNodo.Informacion}";
+            
+        }
+
+        public string InsetarNodoAlFinal(Nodo nuevoNodo)
+        {
+            if (EstaVacia())
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                UltimoNodo.Referencia = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+
+            }
+            return $"Se creo el nodo {nuevoNodo.Informacion}";
+        }
+
+    }
+}
---
+++ PracticaParcial Listas Enlazadas/Server/Nodo.cs
@@ -0,0 +1,24 @@
+﻿namespace PracticaParcial_Listas_Enlazadas.Server
+{
+    public class Nodo
+    {
+            public string Informacion { get; set; }
+            public Nodo? Referencia { get; set;  }
+
+            public Nodo()
+            {
+                Informacion = string.Empty;
+                Referencia = null;
+            }
+            public Nodo(string informacion)
+            {
+                Informacion = informacion;
+                Referencia = null;
+            }
+
+            public override string ToString()
+            {
+                return $"{Informacion}";
+            }
+    }
+}
